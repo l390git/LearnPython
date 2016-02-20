bd2f2368b9a7b845a017a3e0347ebd9890a4c7fa
l390git <steffen@thoss4you.de>
2016-02-20 12:07:09 +0100

    Update import_from _command.py

--- import_from _command.py
+++ import_from _command.py
@@ -2,9 +2,11 @@ import sys
 try:
     x=float(sys.argv[1])
     y=float(sys.argv[2])
+    z=float(sys.argv[3])
     print("Programmname:", sys.argv[0])
     print("Erster Parameter:", x)
-    print("Erster Parameter:", y)
-    print("Ergebniss:", x+y)
+    print("Zweiter Parameter:", y)
+    print("Dritter Parameter:", z)
+    print("Ergebniss:", x+y+z)
 except:
     print("Bitte nur Zahlen eingeben !")
