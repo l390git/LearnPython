6952abf270da9b152236adf19e65267eb2e60db9
Steffen <steffen@thoss4you.de>
2017-12-10 17:47:04 +0100

    added trey and except

--- python_book/Zahlenspiel.py
+++ python_book/Zahlenspiel.py
@@ -1,9 +1,16 @@
 #Imports
 import random
+import os
 
 #Init Zufallsgenerator
 random.seed()
 
+#Init Variables
+fehler = 1
+versuch=0
+
+#Clear Screen
+os.system('clear')
 
 # Werte
 a = random.randint(1,10)
@@ -11,27 +18,30 @@ b = random.randint(1,10)
 
 # Berechung
 c = a + b
-
-#Init Zahl
 zahl=c+1
 
-#Init Versuch
-versuch=0
 
 print("Aufgabe:", a, "+", b)
 #Schleife mit 4 durchläufen
 while zahl != c:
     versuch=versuch+1
     #Eingabe
-    print("Bitte geben Sie eine Zahl ein:")
-    z=input()
-    zahl=int(z)
+    while fehler == 1:
+        print("Bitte geben Sie eine Zahl ein:")
+        z=input()
+        try:
+            zahl=int(z)
+            fehler = 0
+        except:
+            print("Sie haben keine ganze Zahl eingegeben")
+
     #Verzweigung
     if zahl == c:
         print("Zahl ist richtig")
         break
     else:
         print("Zahl ist falsch")
+    fehler = 1
 
 #Ende
 print("Ergebnis:", c)
