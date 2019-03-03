ee9541739f03da0d223bc0b50cd4edd1b2d464ae
Steffen <steffen@thoss4you.de>
2019-03-03 18:04:19 +0100

    enhanced with displaying day, hour, minutes and seconds

--- practice_python_tasks/1-how-long-to-live.py
+++ practice_python_tasks/1-how-long-to-live.py
@@ -11,6 +11,9 @@ age=[0,0]
 #Clear screen
 os.system('clear')
 
+#State what this program is about
+print("This little program will ask for your name, current age and target age. It will show you how much time is left")
+
 #Input name, age and target age
 print("What is your name:")
 name=input()
@@ -28,13 +31,17 @@ for i in range(0, len(age)):
         print("You did not specify a number as your age")
         exit()
 
-
-#State what this program is about
-print("This little program will ask for your name, current age and target age. It will show you how much time is left")
-
-
 #get the current time
 now=datetime.datetime.now()
+#hoe many years until target age as base
+left_years=age[1]-age[0]
+
+#Check if target age is reasonable
+if left_years <= 0:
+    print("Target age must be higher than your current age of",age[0],"years")
+    exit()
 
 #Output result
-print(name, "you will get 100 years old in",now.year+(age[1]-age[0]),"!")
+print(name, "you will get",age[1],"years old in",now.year+left_years,"!")
+print("You have",left_years,"years,",left_years*365,"days and",left_years*365*24,"hours left !")
+print("These are",left_years*365*24*60,"minutes and",left_years*365*24*60*60,"seconds!")
