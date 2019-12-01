dd17981aba2a7aec75de179ce1db6d3cd6a6081f
Steffen <steffen@thoss4you.de>
2019-12-01 18:15:33 +0100

    Reformat file to better coding style

--- practice_python_tasks/1-100-years-old.py
+++ practice_python_tasks/1-100-years-old.py
@@ -4,29 +4,29 @@
 import os
 import datetime
 
-#Init variables
-age=0
+# Init variables
+age = 0
 
-#Clear screen
+# Clear screen
 os.system('clear')
 
-#State what this program is about
+# State what this program is about
 print("This little program will ask for your name and age and tell you in which year you will turn 100!")
 
-#Input name and age
+# Input name and age
 print("What is your name:")
-name=input()
+name = input()
 
 print("Please tell me your age:")
-z=input()
+z = input()
 try:
-    age=int(z)
+    age = int(z)
 except:
     print("You did not specify a number as your age")
     exit()
 
-#get the current time
-now=datetime.datetime.now()
+# get the current time
+now = datetime.datetime.now()
 
-#Output result
-print(name, "you will get 100 years old in",now.year+(100-age),"!")
+# Output result
+print(name, "you will get 100 years old in", now.year + (100 - age), "!")
